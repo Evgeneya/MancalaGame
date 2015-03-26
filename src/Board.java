6c4e37ebfb004f652d7fa0edb6163eca832c85d4
Kuvshinova <lenochka_kuvshinova@mail.ru>
2015-03-26 22:26:29 +0300

    new save

--- src/Board.java
+++ src/Board.java
@@ -2,11 +2,11 @@
  * Created by 1 on 18.03.2015.
  */
 
-/* true - user, false - comp or 2-user */
+/* 1 - user, 0 - comp or 2-user */
 
 public class Board {
     private int[][] board;
-    private int MyMancala, YouMancala;     
+    private int MyMancala, YouMancala;
 
     public Board(){
         board = new int[2][6];
@@ -23,29 +23,60 @@ public class Board {
         /* niche - номер ниши, с которой начнём раскладывать камни */
         return true;
     }
-    //Для пользователя, параметр player для полиморфизма + на случай игры двух игроков
+    //Для пользователя, параметр player для полиморфизма
     public boolean Step(int niche, byte player){
         int index=niche-1;
         boolean flag=false;
         int sum=board[player][index];
         boolean end=false;      //индикатор границы своего поля
+        boolean gear=false;
         board[player][index]=0;
         index++;
         while (sum!=0){
-            flag=false;
-            if (index<6){
-                sum--;
-                board[player][index]++;
-                index++;
+            while (sum!=0) {
+                flag = false;
+                if (index < 6) {
+                    sum--;
+                    board[player][index]++;
+                    index++;
+                } else {
+                    sum--;
+                    end = true;
+                    YouMancala++;
+                    break;
+                }
             }
-            else
-            {
-                sum--;
-                end=true;
-
+            if (sum!=0) {
+                index=5;
+                while(sum!=0){
+                    if (index>=0){
+                        flag=true;
+                        sum--;
+                        board[player-1][index]++;
+                        index--;
+                    }
+                    else{
+                        sum--;
+                        break;
+                    }
+                }
             }
         }
-        return true;
+        if (!flag){
+            if (!end){
+                if(board[player][index-1]!=1){
+                    Step(index,player);
+                }
+                else{
+                    YouMancala+=board[player-1][index-1];
+                    board[player-1][index-1]=0;
+                    return gear;
+                }
+            }
+            else
+                gear=true;
+        }
+        return gear;
     }
 
     public int[][] getBoard() {
