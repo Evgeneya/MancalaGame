0ef3aa2efddc4794432cafc0ddfd043dd7d7a625
Kuvshinova <lenochka_kuvshinova@mail.ru>
2015-03-27 17:50:42 +0300

    new save

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,13 @@
  * Created by 1 on 18.03.2015.
  */
 public class Game {
+    static int board[][];
     public static void main(String[] args) {
-
+        Board board=new Board();
+        board.print();
+        board.Step(3,1);
+        board.print();
+        board.Step(5);
+        board.print();
     }
 }
