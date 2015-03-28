d969111d276077551c358b901152ee9ff63ca6c8
Evgeneya <Evgeneya>
2015-03-28 15:15:52 +0300

    Добавила различные функции в класс Tree и Comp надо разобраться как передавать переменную простого типа по ссылке. Женя

--- src/Comp.java
+++ src/Comp.java
@@ -1,13 +1,30 @@
 /**
  * Created by 1 on 18.03.2015.
  */
-public class Comp {
-    private boolean player = true;
-    /*public void Move(Board board){
-        Tree tree = new Tree();
-        int[][] mas = board.getBoard();
-        tree.AssessTree(player, mas, (byte) 0);
-        byte niche = tree.BestStep();
-        board.Step(niche, player);
-    }*/
+public class Comp extends Tree{
+
+    private boolean player = false;
+    private int best;
+
+    public Comp(int step, Board board) {
+        super(step);
+        AssessTree(getRoot(), player, null, 0, board);
+        this.best = BestStep(getRoot(), board);
+    }
+
+    public int getBest() {
+        return best;
+    }
+
+    int BestStep(TreeNode root, Board board)
+    {
+        int [] MasAssess = AssessStep(root, board);
+        int IndexMaxMark = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            if (MasAssess[IndexMaxMark] < MasAssess[i])
+                IndexMaxMark = i;
+        }
+        return IndexMaxMark;		//Ход, при котором предполагаемое число камней, которые могут быть положены с свою манкалу, больше
+    }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -2,152 +2,115 @@
 /**
  * Created by 1 on 18.03.2015.
  */
-public class Tree{
-
-    public static void main(String[] args) {
-        Tree tree = new Tree(1);  //передаем шаг игрока
-        //System.out.println(tree.root.getNiche6().getNiche3().getNiche1().getNiche1().getNiche1().getNiche1().getNiche());
-        //System.out.println(tree.root.getNiche());
-
-        int mas[][] = null;
-        Board board = new Board();
-        tree.AssessTree(tree.root, false, mas, 0, board);
-        System.out.println(tree.root.getMark());
-    }
+public class Tree {
 
     private TreeNode root;
 
-    public Tree(int step){    //create tree
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public Tree(int step) {    //create tree
         this.root = new TreeNode(step, 0);  //0-level
     }
 
-    public void Delete(){
+    public void Delete() {
 
     }
 
-    public int AssessVertex(int step, boolean player, /*менять?*/Integer mark, int[][] dop, Boolean gear){
+    public int AssessVertex(int step, boolean player, /*менять?*/Integer mark, int[][] dop, Boolean gear) {
         gear = false;
         int index = step - 1;
         int pl = player ? 1 : 0;  //для удобства использования в дальнейшем
         int sum = dop[pl][index];
         if (sum == 0)
             return 0;
-        dop[pl][index]=0;
-        boolean flag = false;		//Перешёл ход на поле противника или нет
+        dop[pl][index] = 0;
+        boolean flag = false;        //Перешёл ход на поле противника или нет
         boolean end = false;
-        if (pl == 0)
-        {
+        if (pl == 0) {
             index--;
-            while (sum!=0)
-            {
+            while (sum != 0) {
                 flag = false;
-                if (index>=0)
-                {
+                if (index >= 0) {
                     sum--;
                     dop[0][index]++;
                     index--;
-                }
-                else
-                {
+                } else {
                     sum--;
                     end = true;
                     mark++;
                     break;
                 }
             }
-            if (sum != 0)
-            {
+            if (sum != 0) {
                 index = 0;
-                while (sum != 0)
-                {
-                    if (index < 6)
-                    {
+                while (sum != 0) {
+                    if (index < 6) {
                         flag = true;
                         sum--;
                         dop[1][index]++;
                         index++;
-                    }
-                    else
-                    {
+                    } else {
                         sum--;
                         break;
                     }
                 }
             }
-            if (flag == false)		//Ход остался на своём поле
+            if (flag == false)        //Ход остался на своём поле
             {
-                if (end == false)
-                {
+                if (end == false) {
                     //if ((dop[0][index+1]!=0)&&(dop[0][index+1]!=1))
-                    if (dop[0][index+1] > 1)
-                    {
-                        AssessVertex(index+2, false, mark, dop, gear);
-                    }
-                    else
-                    {
-                        mark += dop[1][index+1];
-                        dop[1][index+1] = 0;
+                    if (dop[0][index + 1] > 1) {
+                        AssessVertex(index + 2, false, mark, dop, gear);
+                    } else {
+                        mark += dop[1][index + 1];
+                        dop[1][index + 1] = 0;
                         return mark;
                     }
-                }
-                else
-                    gear = true;		//Последний камень попал в манкалу, значит игрок не передаёт ход ,снова ходит из любой своей вершины
+                } else
+                    gear = true;        //Последний камень попал в манкалу, значит игрок не передаёт ход ,снова ходит из любой своей вершины
+            } else {
+                mark--;        //Если ход перешёл к противнику вычитаем 1 из оценки, т.к.выгодне,если ход останется на поле компьютера
             }
-            else
-            {
-                mark--;		//Если ход перешёл к противнику вычитаем 1 из оценки, т.к.выгодне,если ход останется на поле компьютера
-            }
-        }
-        else
-        {
+        } else {
             index++;
-            while (sum != 0)		//Ход на поле игрока
+            while (sum != 0)        //Ход на поле игрока
             {
                 flag = false;
-                if (index < 6)
-                {
+                if (index < 6) {
                     sum--;
                     dop[1][index]++;
                     index++;
-                }
-                else
-                {
+                } else {
                     sum--;
                     end = true;
                     break;
                 }
             }
-            if(sum != 0)
-            {
+            if (sum != 0) {
                 index = 5;
-                while (sum != 0)	//Ход на поле компьютера
+                while (sum != 0)    //Ход на поле компьютера
                 {
-                    if (index>=0)
-                    {
-                        flag = true;		//Ход перешёл на поле противника
+                    if (index >= 0) {
+                        flag = true;        //Ход перешёл на поле противника
                         sum--;
                         dop[0][index]++;
                         index--;
-                    }
-                    else
-                    {
+                    } else {
                         sum--;
                         break;
                     }
                 }
             }
-            if (flag == false)		//Ход остался на своём поле
+            if (flag == false)        //Ход остался на своём поле
             {
-                if (end == false)
-                {
+                if (end == false) {
                     //if ((dop[1][index-1]!=0)&&(dop[1][index-1]!=1))
-                    if (dop[1][index-1] > 1)
-                    {
+                    if (dop[1][index - 1] > 1) {
                         AssessVertex(index, true, mark, dop, gear);
-                    }
-                    else
-                    {
-                        dop[0][index-1] = 0;
+                    } else {
+                        dop[0][index - 1] = 0;
                         return mark;
                     }
                 }
@@ -158,85 +121,124 @@ public class Tree{
         return mark;
     }
 
-    public void AssessTree (TreeNode cur, boolean player, int[][] mas, int level, Board board){
-        if (level>5)
-        {
+    public void AssessTree(TreeNode cur, boolean player, int[][] mas, int level, Board board) {
+        if (level > 5) {
             return;
         }
         int mark = 0;
-        boolean gear = false;		//Индикатор закончился ход на манкале или нет
+        boolean gear = false;        //Индикатор закончился ход на манкале или нет
         int dop1[][] = board.getBoard();
-        int dop2 [][] = dop1;  //для инициализации приравняла null
-        if (level != 0)
-        {
-            if ((player == false)&&(dop1[0][cur.getNiche() - 1] == 0))			//Не оцениваем пустую нишу
-            return;
+        int dop2[][] = dop1;  //для инициализации приравняла null
+        if (level != 0) {
+            if ((player == false) && (dop1[0][cur.getNiche() - 1] == 0))            //Не оцениваем пустую нишу
+                return;
             cur.setMark(AssessVertex(cur.getNiche(), player, mark, dop1, gear));
         }
-        if (gear != true)	//Если последний камень хода не попал в манкалу, то ход передаётся противнику
+        if (gear != true)    //Если последний камень хода не попал в манкалу, то ход передаётся противнику
         {
             player = !player;
         }
 
         for (int i = 0; i < 2; i++)
-            for(int j = 0; j < 6; j++)
+            for (int j = 0; j < 6; j++)
                 dop2[i][j] = dop1[i][j];
         //dop2 = dop1.clone();
 
-        if ((cur.getNiche1() != null) && (dop1[0][cur.getNiche1().getNiche() - 1] != 0))
-        {
+        if ((cur.getNiche1() != null) && (dop1[0][cur.getNiche1().getNiche() - 1] != 0)) {
             mark = 0;
-            AssessTree(cur.getNiche1(), player, dop1, level+1, board);
+            AssessTree(cur.getNiche1(), player, dop1, level + 1, board);
         }
-        mark=0;
+        mark = 0;
         for (int i = 0; i < 2; i++)
-            for(int j = 0; j < 6; j++)
-                dop1[i][j]=dop2[i][j];
+            for (int j = 0; j < 6; j++)
+                dop1[i][j] = dop2[i][j];
         //dop1 = dop2.clone();
-        if ((cur.getNiche2() != null)&&(dop1[0][cur.getNiche2().getNiche() - 1] != 0))
-        {
-            AssessTree(cur.getNiche2(), player, dop1,level+1, board);
+        if ((cur.getNiche2() != null) && (dop1[0][cur.getNiche2().getNiche() - 1] != 0)) {
+            AssessTree(cur.getNiche2(), player, dop1, level + 1, board);
         }
         for (int i = 0; i < 2; i++)
-            for(int j = 0; j < 6; j++)
-                dop1[i][j]=dop2[i][j];
+            for (int j = 0; j < 6; j++)
+                dop1[i][j] = dop2[i][j];
 
         mark = 0;
-        if (cur.getNiche3() != null)
-        {
-            AssessTree(cur.getNiche3(),player, dop1, level+1, board);
+        if (cur.getNiche3() != null) {
+            AssessTree(cur.getNiche3(), player, dop1, level + 1, board);
         }
 
         for (int i = 0; i < 2; i++)
-            for(int j = 0; j < 6; j++)
-                dop1[i][j]=dop2[i][j];
+            for (int j = 0; j < 6; j++)
+                dop1[i][j] = dop2[i][j];
 
         mark = 0;
-        if (cur.getNiche4() != null)
-        {
-            AssessTree(cur.getNiche4(), player, dop1, level+1, board);
+        if (cur.getNiche4() != null) {
+            AssessTree(cur.getNiche4(), player, dop1, level + 1, board);
         }
         for (int i = 0; i < 2; i++)
-            for(int j = 0; j < 6; j++)
-                dop1[i][j]=dop2[i][j];
+            for (int j = 0; j < 6; j++)
+                dop1[i][j] = dop2[i][j];
         mark = 0;
-        if (cur.getNiche5() != null)
-        {
-            AssessTree(cur.getNiche5(), player, dop1, level+1, board);
+        if (cur.getNiche5() != null) {
+            AssessTree(cur.getNiche5(), player, dop1, level + 1, board);
         }
         for (int i = 0; i < 2; i++)
-            for(int j = 0; j < 6; j++)
-                dop1[i][j]=dop2[i][j];
+            for (int j = 0; j < 6; j++)
+                dop1[i][j] = dop2[i][j];
         mark = 0;
-        if (cur.getNiche6() != null)
-        {
-            AssessTree(cur.getNiche6(), player, dop1, level+1, board);
+        if (cur.getNiche6() != null) {
+            AssessTree(cur.getNiche6(), player, dop1, level + 1, board);
         }
         return;
     }
 
-    public byte BestStep(){
-        return 0;
+    int Sum(TreeNode cur, Integer sum, int level) {
+        if (level > 4) {
+            return sum;
+        }
+        sum += cur.getMark();            //Сумма всех камней, которые возможно получить при совершении хода
+        Sum(cur.getNiche1(), sum, level + 1);
+        Sum(cur.getNiche2(), sum, level + 1);
+        Sum(cur.getNiche3(), sum, level + 1);
+        Sum(cur.getNiche4(), sum, level + 1);
+        Sum(cur.getNiche5(), sum, level + 1);
+        Sum(cur.getNiche6(), sum, level + 1);
+        return sum;
+    }
+
+    int[] AssessStep(TreeNode root, Board board)    //Оценка каждого хода
+    {
+        int[] MasAssess = new int[6];        //Массив оценок ходов
+        int[][] field = board.getBoard();
+        int sum = 0;
+        if (field[0][root.getNiche1().getNiche() - 1] != 0)
+            MasAssess[0] = Sum(root.getNiche1(), sum, 1);
+        else
+            MasAssess[0] = 0;
+        sum = 0;
+        if (field[0][root.getNiche2().getNiche() - 1] != 0)
+            MasAssess[1] = Sum(root.getNiche2(), sum, 1);
+        else
+            MasAssess[1] = 0;
+        sum = 0;
+        if (field[0][root.getNiche3().getNiche() - 1] != 0)
+            MasAssess[2] = Sum(root.getNiche3(), sum, 1);
+        else
+            MasAssess[2] = 0;
+        sum = 0;
+        if (field[0][root.getNiche4().getNiche() - 1] != 0)
+            MasAssess[3] = Sum(root.getNiche4(), sum, 1);
+        else
+            MasAssess[3] = 0;
+        sum = 0;
+        if (field[0][root.getNiche5().getNiche() - 1] != 0)
+            MasAssess[4] = Sum(root.getNiche5(), sum, 1);
+        else
+            MasAssess[4] = 0;
+        sum = 0;
+        if (field[0][root.getNiche6().getNiche() - 1] != 0)
+            MasAssess[5] = Sum(root.getNiche6(), sum, 1);
+        else
+            MasAssess[5] = 0;
+        return MasAssess;
     }
 }
 
@@ -261,10 +263,6 @@ class TreeNode {
         return niche;
     }
 
-    public void setNiche(int niche) {
-        this.niche = niche;
-    }
-
     public int getMark() {
         return mark;
     }
@@ -277,48 +275,24 @@ class TreeNode {
         return niche1;
     }
 
-    public void setNiche1(TreeNode niche1) {
-        this.niche1 = niche1;
-    }
-
     public TreeNode getNiche2() {
         return niche2;
-    }
-
-    public void setNiche2(TreeNode niche2) {
-        this.niche2 = niche2;
     }
 
     public TreeNode getNiche3() {
         return niche3;
     }
 
-    public void setNiche3(TreeNode niche3) {
-        this.niche3 = niche3;
-    }
-
     public TreeNode getNiche4() {
         return niche4;
-    }
-
-    public void setNiche4(TreeNode niche4) {
-        this.niche4 = niche4;
     }
 
     public TreeNode getNiche5() {
         return niche5;
     }
 
-    public void setNiche5(TreeNode niche5) {
-        this.niche5 = niche5;
-    }
-
     public TreeNode getNiche6() {
         return niche6;
-    }
-
-    public void setNiche6(TreeNode niche6) {
-        this.niche6 = niche6;
     }
 }
 
