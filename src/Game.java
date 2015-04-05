7fd5599cd2087361860500b377cbc1ad5e11ebd7
Kuvshinova <lenochka_kuvshinova@mail.ru>
2015-04-05 19:38:33 +0300

    new

--- src/Game.java
+++ src/Game.java
@@ -2,21 +2,31 @@
  * Created by 1 on 18.03.2015.
  */
 public class Game {
-    
     public static void main(String[] args) {
         Board board=new Board();
-        int step;
+        int step=0;
+        boolean gear=true;
         board.print();
+        boolean player=true;
         while (!board.CheckEndGame())
         {
-            System.out.println("Введите номер ниши:");
-            step=board.userInput();
-            board.Step(step, 1);// для пользователя
-            board.print();
-            Comp comp=new Comp(step,board);
-            //System.out.println(comp.getBest());
-            board.Step(3);//для компа
-            board.print();
+            if (player){
+                while (gear) {
+                    System.out.println("Введите номер ниши:");
+                    step = board.userInput();
+                    gear = board.Step(step, 1);
+                    board.print();
+                }
+            }
+            else{
+                while (gear){
+                    Comp comp=new Comp(step,board);
+                    gear=board.Step(comp.getBest());//для компа
+                    board.print();
+                }
+            }
+            player=!player;
+            gear=true;
         }
         board.winner();
     }
