e12c6668011ed674d685a4a9eb05713a3727f14e
Evgeneya <Evgeneya>
2015-05-29 00:53:01 +0300

    Рефакторинг методов (все в комментах)

--- src/Board.java
+++ src/Board.java
@@ -18,160 +18,125 @@ public class Board {
         return this.board[1][i - 1];
     }
 
-    public boolean Step(int hole) {
-        int index = hole - 1;
-        boolean flag = false;
-        int sum = this.board[0][index];
-        if(sum == 0) {
+    /* Для компа или второго пользователя */
+    public boolean Step(int hole){
+        /* hole - номер ниши, с которой начнём раскладывать камни */
+        int index=hole-1;
+        boolean flag=false;   //принимает значение true, если раскладка переходит на поле противника(пользователя)
+        int sum=board[0][index];
+        if (sum == 0)
             return false;
-        } else {
-            boolean end = false;
-            boolean gear = false;
-            this.board[0][index] = 0;
-
-
-            --index;
-
-            label48:
-            while(sum != 0) {
-                while(true) {
-                    if(sum != 0) {
-                        if(index >= 0) {
-                            --sum;
-                            ++this.board[0][index];
-
-
-                            --index;
-                            continue;
-                        }
-
-                        --sum;
-                        end = true;
-                        ++this.CompMancala;
-
+        boolean end=false;      //индикатор попадания в свою манкалу
+        boolean gear=false;
+        board[0][index]=0;
+        index--;
+        //добавить еще 1 цикл, для случая возвращения на свое поле
+        if (sum!=0){  // убрать
+            while(sum!=0){
+                //flag = false //можем вернуться в этот цикл, то есть на свое поле
+                if(index>=0){
+                    sum--;
+                    board[0][index]++; //можно сократить код
+                    index--;
+                }
+                else {
+                    sum--;
+                    end=true;
+                    CompMancala++;
+                    break;
+                }
+            }
+            if (sum!=0){ //убрать
+                index=0;
+                while(sum!=0) {
+                    //end = false;  // можем в следующей итерации попасть на свое поле, но не в манкалу, поэтому сбрасываем true
+                    if (index<6) {
+                        flag=true;
+                        sum--;
+                        board[1][index]++;  //можно сократить код
+                        index++;
                     }
-
-                    if(sum == 0) {
+                    else{
+                        sum--;  //зачем? в манкалу противника камни не раскладываются
+                        //index = 5; // вдруг сумма еще не 0 и нужно продолжить расклад на своем поле
                         break;
                     }
-
-                    index = 0;
-
-                    while(true) {
-                        if(sum == 0) {
-                            continue label48;
-                        }
-
-                        if(index >= 6) {
-                            --sum;
-                            continue label48;
-                        }
-
-                        flag = true;
-                        --sum;
-                        ++this.board[1][index];
-
-
-                        ++index;
-                    }
                 }
             }
-
-            if(!flag) {
-                if(!end) {
-                    if(this.board[0][index + 1] == 1) {
-                        this.CompMancala += this.board[1][index + 1];
-                        this.board[1][index + 1] = 0;
-                        return false;
-                    }
-
-                    this.Step(index + 2);
-                } else {
-                    gear = true;
-                }
-            }
-
-            return gear;
         }
+        if (!flag){  //остались на своем поле
+            if(!end){   //последний камень не в манкале
+                if(board[0][index+1]!=1)
+                    Step(index+2);
+                else{
+                    CompMancala+=board[1][index+1];
+                    board[1][index+1]=0;
+                    return false;
+                }
+            }
+            else
+                gear=true;   //return true
+        }
+        return gear;  //можно убрать переменную, вместо неё return false
     }
-
-    public boolean Step(int hole, int player) {
-        int index = hole - 1;
-        boolean flag = false;
-        int sum = this.board[player][index];
-        if(sum == 0) {
+    //Для пользователя, параметр player для полиморфизма
+    public boolean Step(int hole, int player){
+        int index=hole-1;
+        boolean flag=false;  //переход на чужое поле - true
+        int sum=board[player][index];
+        if (sum == 0)
             return false;
-        } else {
-            boolean end = false;
-            boolean gear = false;
-            this.board[player][index] = 0;
-
-            ++index;
-
-            label48:
-            while(sum != 0) {
-                while(true) {
-                    if(sum != 0) {
-                        flag = false;
-                        if(index < 6) {
-                            --sum;
-                            ++this.board[player][index];
-                            ++index;
-                            continue;
-                        }
-
-                        --sum;
-                        end = true;
-                        ++this.YouMancala;
+        boolean end=false;      //индикатор границы своего поля //индикатор попадания в свою манкалу
+        boolean gear=false; // ненужная!
+        board[player][index]=0;
+        index++;
+        //добавить еще 1 цикл, для случая возвращения на свое поле
+        if (sum!=0){ //убрать
+            while (sum!=0) {
+                flag = false;
+                if (index < 6) {
+                    sum--;
+                    board[player][index]++;
+                    index++;
+                } else {
+                    sum--;
+                    end = true;
+                    YouMancala++;
+                    break;
+                }
+            }
+            if (sum!=0) { //убрать
+                index=5;
+                while(sum!=0){
+                    if (index>=0){
+                        flag=true;
+                        sum--;
+                        board[player-1][index]++;
+                        index--;
                     }
-
-                    if(sum == 0) {
+                    else{
+                        sum--;  //зачем?
+                        //index = 0;
                         break;
                     }
-
-                    index = 5;
-
-                    while(true) {
-                        if(sum == 0) {
-                            continue label48;
-                        }
-
-                        if(index < 0) {
-                            --sum;
-                            continue label48;
-                        }
-
-                        flag = true;
-                        --sum;
-                        ++this.board[player - 1][index];
-                        --index;
-
-                    }
                 }
             }
-
-            if(!flag) {
-                if(!end) {
-                    if(this.board[player][index - 1] == 1) {
-                        this.YouMancala += this.board[player - 1][index - 1];
-
-
-
-                        this.board[player - 1][index - 1] = 0;
-
-
-
-                        return gear;
-                    }
-
-                    this.Step(index, player);
-                } else {
-                    gear = true;
-                }
-            }
-
-            return gear;
         }
+        if (!flag){
+            if (!end){
+                if(board[player][index-1]!=1){
+                    Step(index,player);
+                }
+                else{
+                    YouMancala+=board[player-1][index-1];
+                    board[player-1][index-1]=0;
+                    return gear; //return false
+                }
+            }
+            else
+                gear=true; //return true
+        }
+        return gear; //можно убрать переменную, вместо неё return false
     }
 
     public int[][] getBoard() {
